fe177ed969bb1c78a9b28b4fb7287709df58103f
aaronminaya0 <aaronminaya0@gmail.com>
2022-12-01 00:25:25 -0500

    created commentsController.js, commentsRoutes.js. Completed comments MVC.

--- model/commentsModel.js
+++ model/commentsModel.js
@@ -1,12 +1,14 @@
-const { pathToFileURL } = require('url');
 const pool = require('../db.js');
 
 class Comments {
-    static grabComments() {
-        return pool.query('SELECT * FROM comments JOIN post on comments.post_id = post.post_id')
+    static grabCommentsFromDB(post_id) {
+        return pool.query('SELECT * FROM comments JOIN post on comments.post_id = $1', [post_id])
     }
 
-    static createComment(){
-        
+    static createCommentToDB(user_id, post_id, likes, comment){
+        return pool.query('INSERT INTO commets (user_id, post_id, likes, comment) VALUE ($1, $2, $3, $4) RETURNING *', [user_id, post_id, likes, comment])
     }
 }
+
+
+module.exports = Comments
---
+++ routes/commentsRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const commentsRouter = express.Router()
+const commentController = require('../controller/commentsController.js')
+
+commentsRouter.get("/comments", commentController.getAllCommentsForPost)
+
+commentsRouter.post("/new_comment", commentController.createComment)
+
+module.exports = commentsRouter
