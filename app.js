62bd1855dc9e63fa6cc6f5b357963ee0719f7f0c
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2022-12-08 17:53:49 -0500

    uncommented post router and removed old query

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // app.use(commentsRouter)
-// app.use(postRouter)
+app.use(postRouter)
 app.use(userRouter)
 
 
--- controller/userController.js
+++ controller/userController.js
@@ -28,7 +28,6 @@ const addUserInfo = async (request, response) => {
 
 module.exports = {
     getAllUsers,
-    getUserById,
     loginAuthentication,
     addUserInfo
 }
