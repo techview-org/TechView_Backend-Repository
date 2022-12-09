a0396a1878c6b530b8dd7b43bcc680d17402bffd
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2022-12-09 02:55:23 -0500

    linting

--- model/commentsModel.js
+++ model/commentsModel.js
@@ -1,11 +1,11 @@
 const pool = require('../db.js')
 
 class Comments {
-  static grabCommentsFromDB(post_id) {
+  static grabCommentsFromDB (post_id) {
     return pool.query('SELECT * FROM comments WHERE post_id = $1', [post_id])
   }
 
-  static createCommentToDB(user_id, post_id, likes, comment) {
+  static createCommentToDB (user_id, post_id, likes, comment) {
     return pool.query('INSERT INTO commets (user_id, post_id, likes, comment) VALUE ($1, $2, $3, $4) RETURNING *', [user_id, post_id, likes, comment])
   }
 }
--- model/postModel.js
+++ model/postModel.js
@@ -1,19 +1,19 @@
 const pool = require('../db.js')
 
 class Post {
-  static grabPostFromDB() {
+  static grabPostFromDB () {
     return pool.query('SELECT users.username, post.* FROM users, post WHERE post.user_id = users.id')
   }
 
-  static grabFilteredPostsFromDB(filter) {
+  static grabFilteredPostsFromDB (filter) {
     return pool.query('SELECT users.username, post.* FROM users, post WHERE post.post_type = $1 AND users.id = post.user_id', [filter])
   }
 
-  static grabLatestPostIdFromDB() {
+  static grabLatestPostIdFromDB () {
     return pool.query('SELECT MAX(post_id) FROM post')
   }
 
-  static addPostToDB(newPostId, user_id, post_title, post_description, post_type) {
+  static addPostToDB (newPostId, user_id, post_title, post_description, post_type) {
     return pool.query('INSERT INTO post (post_id, user_id, post_title, post_description, post_type) VALUES ($1, $2, $3, $4, $5) RETURNING *', [newPostId, user_id, post_title, post_description, post_type])
   }
 }
