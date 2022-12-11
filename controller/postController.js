2615384b4e27060ad75458e6ec82a0756cb39a42
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2022-12-11 17:21:56 -0500

    added routes for updating likes on post

--- controller/postController.js
+++ controller/postController.js
@@ -24,8 +24,28 @@ const getFilteredPosts = async (request, response) => {
   response.send(filteredPosts)
 }
 
+const updateLikes = async (request, response) => {
+  const postInfo = request.body
+  const username = postInfo.username
+  const postTitle = postInfo.postTitle
+  const isLiked = postInfo.isLiked
+
+  const data = await Post.grabPostInfoFromDB(username, postTitle)
+  const postId = data.rows[0].post_id
+
+  let postWithUpdatedLikes = null
+  if (isLiked) {
+    postWithUpdatedLikes = await Post.subtractLikesForGivenPostInDB(postId)
+  } else {
+    postWithUpdatedLikes = await Post.addToLikesForGivenPostInDB(postId)
+  }
+
+  response.send(postWithUpdatedLikes.rows[0])
+}
+
 module.exports = {
   getPost,
   getFilteredPosts,
-  addPost
+  addPost,
+  updateLikes
 }
--- model/postModel.js
+++ model/postModel.js
@@ -14,7 +14,19 @@ class Post {
   }
 
   static addPostToDB (newPostId, user_id, post_title, post_description, post_type) {
-    return pool.query('INSERT INTO post (post_id, user_id, post_title, post_description, post_type) VALUES ($1, $2, $3, $4, $5) RETURNING *', [newPostId, user_id, post_title, post_description, post_type])
+    return pool.query('INSERT INTO post (post_id, user_id, post_title, post_description, post_type, likes) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [newPostId, user_id, post_title, post_description, post_type, 0])
+  }
+
+  static grabPostInfoFromDB (username, postTitle) {
+    return pool.query('SELECT users.username, post.* FROM users, post WHERE users.id = post.user_id AND users.username = $1 AND post.post_title = $2;', [username, postTitle])
+  }
+
+  static addToLikesForGivenPostInDB(postId) {
+    return pool.query('UPDATE post SET likes = likes + 1 WHERE post_id = $1 RETURNING *', [postId])
+  }
+
+  static subtractLikesForGivenPostInDB (postId) {
+    return pool.query('UPDATE post SET likes = likes - 1 WHERE post_id = $1 RETURNING *', [postId])
   }
 }
 
